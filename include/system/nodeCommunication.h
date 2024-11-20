2bb761096cf2c02069459faeddd4fc3c0d70789f
zhangyongjing <649894200@qq.com>
2024-11-20 10:56:54 +0000

    添加节点之间通信管道

--- include/system/nodeCommunication.h
+++ include/system/nodeCommunication.h
@@ -11,41 +11,41 @@ namespace zrcsSystem {
     template<class T>
     class NodeCommunicaion
     {   private:
-         std::pmr::monotonic_buffer_resource resource;
-         std::pmr::vector<int> vec;
+         std::pmr::monotonic_buffer_resource* resource;
+         std::pmr::vector<T>* vec;
          public:
-         NodeCommunicaion(std::string ,int direction, int size):resource(size),vec(&resource)
+         NodeCommunicaion(std::string ,int direction, int size)
          {
-                     
+                       resource = new std::pmr::monotonic_buffer_resource(size);
+                       vec = new std::pmr::vector<T>(resource);
          }
           int size()
              {
-                return vec.size();
+                return vec->size();
              }
           int read(T& value)
           {  
               T result;
-              if (!vec.empty()) 
+              if (!vec->empty()) 
               {
-                 result=vec.front();
+                 result=vec->front();
                  value=result;
-                 vec.erase(vec.begin());
+                 vec->erase(vec->begin());
               }
               result -1;
           }
           void wirte(T value)
           {
-                 vec.push_back(value);
+                 vec->push_back(value);
           }
 
          ~NodeCommunicaion()
-         {     
-               
+         {    
+            delete vec; 
+            delete resource;              
          }
          
 
     };
-
-
 }
 #endif
