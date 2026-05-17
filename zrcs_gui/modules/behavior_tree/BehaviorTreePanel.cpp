3caabb1334e5a7044969189bc34a7ff5e369f818
zhangyongjing <649894200@qq.com>
2026-05-17 23:04:02 +0800

    修复qt 6.2编译问题

--- zrcs_gui/modules/behavior_tree/BehaviorTreePanel.cpp
+++ zrcs_gui/modules/behavior_tree/BehaviorTreePanel.cpp
@@ -291,12 +291,24 @@ void BehaviorTreePanel::loadFromXML(const QString &xml_text)
 {
     QDomDocument document;
     try {
+#if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
         auto result = document.setContent(xml_text);
         if (!result) {
             throw std::runtime_error(
                 tr("解析XML错误 (第%1行): %2")
                     .arg(result.errorLine).arg(result.errorMessage).toStdString());
         }
+#else
+        QString errorMessage;
+        int errorLine = 0, errorColumn = 0;
+        const bool result = document.setContent(xml_text, &errorMessage, &errorLine, &errorColumn);
+        Q_UNUSED(errorColumn);
+        if (!result) {
+            throw std::runtime_error(
+                tr("解析XML错误 (第%1行): %2")
+                    .arg(errorLine).arg(errorMessage).toStdString());
+        }
+#endif
 
         std::vector<QString> registered_ID;
         for (const auto &it : _treenodeModels) {
